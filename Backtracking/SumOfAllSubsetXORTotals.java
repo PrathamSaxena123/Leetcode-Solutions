05ab5674db6d4ea112a99a5dbec3b37a4ad295ca
Pratham Saxena <prathamsaxena315@gmail.com>
2026-06-09 23:39:56 +0530

    Day 62: Sum of all Subset XOR totals (LeetCode - Easy)

---
+++ Backtracking/SumOfAllSubsetXORTotals.java
@@ -0,0 +1,40 @@
+class Solution {
+
+    public int subsetXORSum(int[] nums) {
+
+        // Start backtracking from index 0
+        // with current XOR = 0
+        return backtrack(nums, 0, 0);
+    }
+
+    private int backtrack(int[] nums,
+                          int index,
+                          int currentXOR) {
+
+        // Base case:
+        // One complete subset has been formed
+        if (index == nums.length) {
+            return currentXOR;
+        }
+
+        // Choice 1:
+        // Include current element in subset
+        int include =
+            backtrack(
+                nums,
+                index + 1,
+                currentXOR ^ nums[index]
+            );
+
+        // Choice 2:
+        // Exclude current element from subset
+        int exclude =
+            backtrack(
+                nums,
+                index + 1,
+                currentXOR
+            );
+
+        // Sum contributions from both choices
+        return include + exclude;
+    }
